cc6f1d7522c46d7f1c5acff010dd3abb554ee102
cuiyuqiang@juzix.io <1qazxsw23edc>
2018-01-21 09:10:21 +0800

    单测

--- jhpt-server/src/test/java/com/tyj/jhpt/server/util/DeviceMsgUtilsTest.java
+++ jhpt-server/src/test/java/com/tyj/jhpt/server/util/DeviceMsgUtilsTest.java
@@ -7,9 +7,7 @@ package com.tyj.jhpt.server.util;
 import com.github.fartherp.framework.common.util.ISOUtil;
 import org.testng.annotations.Test;
 
-import java.math.BigInteger;
-
-import static org.testng.Assert.*;
+import java.util.Date;
 
 /**
  * Created by IntelliJ IDEA.
@@ -20,17 +18,41 @@ import static org.testng.Assert.*;
 public class DeviceMsgUtilsTest {
     @Test
     public void testResolveTime() throws Exception {
-        String time = "07E10C0908380E";
+        String time = "12010E091C00";
         byte[] bytes = ISOUtil.hex2byte(time);
-        DeviceMsgUtils.resolveTime(bytes, 0);
-        String date = "20171109085614";
+        Date d = DeviceMsgUtils.resolveTime(bytes, 0);
+        String date = "171214160810";
+        String s = Integer.toHexString(12);
+        System.out.println(s);
+        Integer i = Integer.parseInt("001F", 16);
+        System.out.println(i);
+        String vin = "LRDS6PEB3HR002350";
+        byte[] b = vin.getBytes("GBK");
+        String vinb = ISOUtil.hexString(b);
+        System.out.println(vinb);
+        String sb = new String(b, "GBK");
+        System.out.println(sb);
 
+        byte bc = 38;
+        System.out.println(0xff & bc);
 
+        byte[] a = "FE".getBytes();
+        System.out.println(1);
+    }
+
+    @Test
+    public void testGetTime() {
+        byte[] bytes = DeviceMsgUtils.getTime();
+        System.out.println(1);
     }
 
     @Test
     public void testReadLatLongInfo() throws Exception {
-
+        String s = "4C52445336504542334852303032333530";
+        byte[] b = s.getBytes();
+        byte[] bytes = ISOUtil.hex2byte(s);
+        String a = new String(bytes, "GBK");
+        System.out.println(a);
     }
 
     @Test
